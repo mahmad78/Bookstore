884d14bb38aa95898509205f025611da90387dc6
mahmad78 <immunir78@gmail.com>
2020-10-16 15:04:34 +0500

    Repository and controller creation along with view Get all books

---
+++ Bookstore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public ViewResult GetAllbooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View(data);
+        }
+
+        //public ViewResult GetBook(int Id)
+        //{
+        //    var oBook = new BookModel { Id=5, Title="Vb", Author="Saif" };
+        //    return_bookRepository.GetBookById(Id);
+        //}
+
+        public List<BookModel> SearchBooks(string title, string author)
+        {
+            return _bookRepository.SearchBook(title,author);
+        }
+    }
+}
---
+++ Bookstore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return Datasource();
+        }
+
+        public BookModel GetBookById(int Id)
+        {
+            return Datasource().Where(b => b.Id == Id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string title, string author)
+        {
+            return Datasource().Where(x => x.Title == title && x.Author == author).ToList();
+        }
+
+        private  List<BookModel> Datasource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ Id=1, Title="MVC", Author="Nitsh"},
+                new BookModel(){ Id=2, Title="CSharpt", Author="xyz"}
+            };
+        }
+    }
+}
